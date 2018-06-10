837ddd4e0e4af5d1c9a235a42dd22987cd49e96b
patrykpp <patryk583@vp.pl>
2018-06-10 19:47:36 +0200

    lab7

---
+++ lab7/Assets/Scripts/ParticleManagement2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManagement2 : MonoBehaviour
+{
+
+    ParticleSystem particles;
+    // Use this for initialization
+    void Start()
+    {
+        particles = gameObject.GetComponent<ParticleSystem>();
+        particles.enableEmission = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            particles.enableEmission = !particles.enableEmission;
+        }
+    }
+}
